1a556eb88b34271b5d67d3001e152e95abaccee1
z1kary <vicburet@orange.fr>
2021-05-18 07:19:24 +0200

    commit

---
+++ uploads/pp/CSS Gradient — Generator, Maker, and Background_files/config.js
@@ -0,0 +1,8 @@
+
+window._mtm = window._mtm || {};
+window._mtm.projectId = "prj:17939cfe-430b-41b9-b63a-a3ce3f1f21fe";
+window._mtm.configAutoblocking =
+{
+    "rules": [],
+    "autoblockingEnabled": false
+}
